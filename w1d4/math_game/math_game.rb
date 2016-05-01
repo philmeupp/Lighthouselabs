145ee9fa5b526601d3e39832a306f1eb804869af
philmeupp <p.hong88@hotmail.com>
2016-05-01 09:54:19 +0000

    wow..what a clusterfuck..

--- w1d4/math_game/math_game.rb
+++ w1d4/math_game/math_game.rb
@@ -1,10 +1,13 @@
+require 'colorize'
+
 def generate_numbers
   @num1 = rand(1..20)
   @num2 = rand(1..20)
   @operator = [:+, :-, :*, :/].sample
   @sum = @num1.send(@operator, @num2)
-  puts "What is #{@num1} #{@operator} #{@num2} = ? "
-  # puts @sum
+  puts "What is #{@num1} #{@operator} #{@num2} equal to? "
+  puts  
+  puts @sum
 end
 
 # def player_details
@@ -15,50 +18,88 @@ end
 # end
 
 def player_name
-  puts "input your names"
+  puts "Input your names"
   print "Player 1: "
   name1 = gets.chomp
   print "Player 2: "
   name2 = gets.chomp
-  @p1 = name1 
-  @p2 = name2
+  @p1 = name1.cyan   
+  @p2 = name2.yellow
 end
 
+# def restart?
+#   puts "Do you want to play again? yes/no "
+#   user_input = gets.chomp.lowercase
+#     case user_input
+#     when "yes" 
+#       # do something
+#     when "no"
+#       puts "WORKED"
+#       @gameover = true
+#     end
+# end
 
 def start_game
   player_name
-  gameover = false
-  while !gameover  
-    print "\n"
-    generate_numbers
-    print "#{@p1}: "
-    user_input = gets.chomp
-    if user_input.to_i == @sum  
-      @player_1_score += 1
-      puts "correct your score is #{@player_1_score}"
+  @gameover = false
+  while !@gameover 
       print "\n"
-    else
-      @player_1_life -= 1
-      puts "wrong you have #{@player_1_life} lives left"
-      print "\n"
-      return gameover = true if @player_1_life == 0
-    end
+      generate_numbers
+      print "#{@p1}: "
+      user_input = gets.chomp
+        if user_input.to_i == @sum  
+          @player_1_score += 1
+          puts "CORRECT! your score is #{@player_1_score}".blue
+          print "\n"
+        else
+          @player_1_life -= 1
+          puts "WRONG! you have #{@player_1_life} lives left".red
+          print "\n" 
+          if @player_1_life <= 0
+            print "Restart? y/n  ".light_red
+            user_input = gets.chomp.downcase
+            if user_input == "n"
+              return @gameover = true
+            elsif user_input == "y"
+              @player_2_life = 3
+              @player_1_life = 3
+              puts "The current score is:"
+              puts "#{@p1}: #{@player_1_score}".cyan
+              puts "#{@p2}: #{@player_2_score}".yellow
+              puts "\n"
+            end
+          end
+        end
 
-    generate_numbers
-    print "#{@p2}: "
-    user_input = gets.chomp
-    if user_input.to_i == @sum
-      @player_2_score += 1
-      puts "correct your score is #{@player_2_score}"
-      print "\n"
-    else
-      @player_2_life -= 1
-      puts "wrong you have #{@player_2_life} lives left"
-      print "\n"
-      gameover = true if @player_2_life == 0
+        generate_numbers
+        print "#{@p2}: "
+        user_input = gets.chomp
+        if user_input.to_i == @sum
+          @player_2_score += 1
+          puts "CORRECT! your score is #{@player_2_score}".blue
+          print "\n"
+        else
+          @player_2_life -= 1
+          puts "WRONG! you have #{@player_2_life} lives left".red
+          print "\n"
+          if @player_2_life <= 0
+            print "Restart? y/n  ".light_red
+            user_input = gets.chomp.downcase
+            if user_input == "n"
+              return @gameover = true
+            elsif user_input == "y"
+              @player_2_life = 3
+              @player_1_life = 3
+              puts "The current score is:"
+              puts "#{@p1}: #{@player_1_score}".cyan
+              puts "#{@p2}: #{@player_2_score}".yellow
+              puts "\n"
+            end
+        end
+      end
     end
   end
-end
+
 
 
 start_game
