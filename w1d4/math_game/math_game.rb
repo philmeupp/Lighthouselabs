91f6e5baa07fb2a785901d0b3c03a4fdc3b42d4d
philmeupp <p.hong88@hotmail.com>
2016-05-01 07:57:21 +0000

    added randomize operator method and player name input method

--- w1d4/math_game/math_game.rb
+++ w1d4/math_game/math_game.rb
@@ -1,8 +1,10 @@
 def generate_numbers
   @num1 = rand(1..20)
   @num2 = rand(1..20)
-  @sum = @num1 + @num2
-  puts @sum
+  @operator = [:+, :-, :*, :/].sample
+  @sum = @num1.send(@operator, @num2)
+  puts "What is #{@num1} #{@operator} #{@num2} = ? "
+  # puts @sum
 end
 
 # def player_details
@@ -12,35 +14,51 @@ end
   @player_2_score = 0
 # end
 
+def player_name
+  puts "input your names"
+  print "Player 1: "
+  name1 = gets.chomp
+  print "Player 2: "
+  name2 = gets.chomp
+  @p1 = name1 
+  @p2 = name2
+end
+
 
 def start_game
-gameover = false
+  player_name
+  gameover = false
   while !gameover  
+    print "\n"
     generate_numbers
-    puts "Player 1: what is #{@num1} + #{@num2} ?"
+    print "#{@p1}: "
     user_input = gets.chomp
-    if user_input.to_i == @sum
+    if user_input.to_i == @sum  
       @player_1_score += 1
       puts "correct your score is #{@player_1_score}"
+      print "\n"
     else
       @player_1_life -= 1
       puts "wrong you have #{@player_1_life} lives left"
-      gameover = true if @player_1_life == 0
+      print "\n"
+      return gameover = true if @player_1_life == 0
     end
+
     generate_numbers
-    puts "Player 2: what is #{@num1} + #{@num2} ?"
+    print "#{@p2}: "
     user_input = gets.chomp
     if user_input.to_i == @sum
       @player_2_score += 1
       puts "correct your score is #{@player_2_score}"
+      print "\n"
     else
       @player_2_life -= 1
       puts "wrong you have #{@player_2_life} lives left"
+      print "\n"
       gameover = true if @player_2_life == 0
     end
   end
 end
 
-# puts generate_numbers
 
 start_game
